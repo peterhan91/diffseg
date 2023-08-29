ce02243e3baf9bce8701d9859dc45ad47cf49965
Tianyu Han <petertianyuhan@hotmail.com>
2023-08-29 13:46:30 +0200

    add training script

---
+++ run_liver.py
@@ -0,0 +1,9 @@
+from templates import *
+from templates_latent import *
+
+if __name__ == '__main__':
+    # 256 requires 8x v100s, in our case, on two nodes.
+    # do not run this directly, use `sbatch run_ffhq256.sh` to spawn the srun properly.
+    gpus = [2]
+    conf = liver_autoenc()
+    train(conf, gpus=gpus)
